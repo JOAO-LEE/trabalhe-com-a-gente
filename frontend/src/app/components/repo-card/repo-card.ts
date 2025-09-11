e653f292e8139eb6c042d68b40fd461815318706
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-11 20:22:50 -0300

    feat: create repository card component

---
+++ frontend/src/app/components/repo-card/repo-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'repo-card',
+  imports: [],
+  templateUrl: './repo-card.html',
+  styleUrl: './repo-card.css'
+})
+export class RepoCard {
+
+}
