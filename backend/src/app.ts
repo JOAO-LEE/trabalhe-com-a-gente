24e6298d8fb860f3d4e861fa354158e1db3e6aca
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-11 14:51:00 -0300

    feat(app): integrate GitHub controller and structure app in layers

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,16 +1,10 @@
 import express, { type Application } from "express";
-import { type Request, type Response } from "express";
+import { githubController } from "./controllers/github/github.controller.ts";
 
 const app: Application = express();
 
 app.use(express.json());
 
-app.get("/", async (req: Request, res: Response) => {
-    const { searchQuery } = req.query;
-    const githubApiUrl = `https://api.github.com/search/repositories?q=${searchQuery}`
-    const response = await fetch(githubApiUrl)
-    const result = await response.json()
-    res.status(200).json(result)
-});
+app.use("/", githubController);
 
 export default app;
