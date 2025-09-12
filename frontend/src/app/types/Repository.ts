0c9c56fe500b203d8eddd83bae7fea8f9c70b789
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-12 19:46:21 -0300

    feat: create repository and api response types

---
+++ frontend/src/app/types/Repository.ts
@@ -0,0 +1,20 @@
+export type Repository = {
+    id: number;
+    name: string;
+    htmlUrl: string;
+    owner: {
+        avatarUrl: string
+        login: string
+    };
+    description: string;
+    watchersCount: number;
+    stargazersCount: number;
+    language: string;
+}
+
+export type RepositoryResult = {
+    pagination: string;
+    totalCount: number
+    incompleteResults: boolean,
+    items: Array<Repository>
+}
