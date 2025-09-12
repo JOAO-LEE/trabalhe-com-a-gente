c16986aebcac26cb4b79177f60a85fd2445362fb
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-12 20:00:41 -0300

    fix: dynamic repo rendering plus error handling

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,27 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderSearch } from './components/header-search/header-search';
+import { RepoCard } from './components/repo-card/repo-card';
+import { GithubService } from './services/github';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderSearch, RepoCard],
   templateUrl: './app.html',
   styleUrl: './app.css'
+
 })
 export class App {
-  protected readonly title = signal('frontend');
+  hasSearched = signal(false);
+  page: number = 1;
+
+  constructor(public githubService: GithubService) { }
+
+  onSearch(query: string) {
+    this.hasSearched.set(true);
+    this.githubService
+      .searchRepositories(query, this.page.toString())
+      .subscribe();
+
+  }
 }
