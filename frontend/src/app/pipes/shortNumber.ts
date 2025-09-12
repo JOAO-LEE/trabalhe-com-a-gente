cb4a0062f22eb2befddfafff9fdeb19384d448a4
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-12 16:49:16 -0300

    feat: create pipe to handle big numbers

---
+++ frontend/src/app/pipes/shortNumber.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'shortNumber'
+})
+export class ShortNumberPipe implements PipeTransform {
+    transform(value: number): string {
+        if (value >= 1000000) {
+            return (value / 1000000).toFixed(1).replace(/\.0$/, '') + 'M';
+        }
+        if (value >= 1000) {
+            return (value / 1000).toFixed(1).replace(/\.0$/, '') + 'k';
+        }
+        return value.toString();
+    }
+}
