e023d45c1fd3d67496633bf91c50eb1b440178d0
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-12 19:40:11 -0300

    feat: adds repository card dynamic rendering

--- frontend/src/app/components/repo-card/repo-card.ts
+++ frontend/src/app/components/repo-card/repo-card.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Repository } from '../../types/Repository';
+import { ShortNumberPipe } from '../../pipes/shortNumber';
 
 @Component({
   selector: 'repo-card',
-  imports: [],
+  imports: [ShortNumberPipe],
   templateUrl: './repo-card.html',
   styleUrl: './repo-card.css'
 })
 export class RepoCard {
+
+  @Input() repository!: Repository;
 
 }
