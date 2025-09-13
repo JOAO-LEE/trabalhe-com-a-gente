2a54dce930060fe95a096b7e8b8c9f1151e25eef
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-13 04:51:53 -0300

    feat: paginator with numbers

--- frontend/src/app/components/paginator/paginator.ts
+++ frontend/src/app/components/paginator/paginator.ts
@@ -40,4 +40,38 @@ export class Paginator {
     return Array.from({ length: this.totalPages }, (_, i) => i + 1);
   }
 
+  getVisiblePages(): (number | string)[] {
+    if (this.totalPages <= 7) return Array.from({ length: this.totalPages }, (_, i) => i + 1)
+
+    const visiblePages: (number | string)[] = [];
+    const current = this.page;
+    const total = this.totalPages;
+
+    visiblePages.push(1);
+
+
+    let startPage = Math.max(2, current - 2);
+    let endPage = Math.min(total - 1, current + 2);
+
+    if (startPage > 2) {
+      visiblePages.push('...');
+    }
+
+
+    for (let i = startPage; i <= endPage; i++) {
+      visiblePages.push(i);
+    }
+
+    if (endPage < total - 1) {
+      visiblePages.push('...');
+    }
+
+    visiblePages.push(total);
+
+    return visiblePages;
+  }
+
+  isEllipsis(page: number | string): boolean {
+    return page === '...';
+  }
 }
