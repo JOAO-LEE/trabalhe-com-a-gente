670c12895815b501357089e0bedfd2e058dcd375
JOAO-LEE <joaumlimaum@gmail.com>
2025-09-13 00:04:15 -0300

    feat: create function to extract last page number

---
+++ backend/src/utils/extractLastPageNumber.ts
@@ -0,0 +1,11 @@
+export function extractLastPageNumber(linkHeader: string | null): number {
+    if (!linkHeader) return 1;
+    const links = linkHeader.split(",");
+    console.log(links)
+    const lastPageLink = links.find((link) => link.includes('rel="last"'));
+    if (!lastPageLink) return 1;
+    const linkSplit = lastPageLink.split("page=")
+    const lastItem = linkSplit.slice(linkSplit.length - 1);
+    const numberOfPages = +lastItem[0].split(">;")[0]
+    return numberOfPages
+}
